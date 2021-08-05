29bc6e4325f9a3fceda163a53d678a5cbe88ac40
joao <joaocapitaneo@gmail.com>
2021-08-05 13:29:30 -0300

    teste

--- app/src/main/java/br/com/app5m/appaps/MainActivity.kt
+++ app/src/main/java/br/com/app5m/appaps/MainActivity.kt
@@ -1,11 +1,18 @@
 package br.com.app5m.appaps
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ThemeControl() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ app/src/main/java/br/com/app5m/appaps/ThemeControl.kt
@@ -0,0 +1,25 @@
+package br.com.app5m.appaps
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
+open class ThemeControl : AppCompatActivity() {
+
+    var sharedPreferences: SharedPreferences? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sharedPreferences = getSharedPreferences("night", 0)
+        val booleanValue = sharedPreferences?.getBoolean("night_mode", false)
+
+        if (booleanValue == true) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+        }
+
+    }
+}
